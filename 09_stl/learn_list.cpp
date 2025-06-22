4d9bc30a6b8462e55b162b028435f49bdaee16fb
ankurbansal15 <ankurbansal153@gmail.com>
2025-06-23 01:32:44 +0530

    Learn List

---
+++ 09_stl/learn_list.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <list>
+
+using namespace std;
+int main() {
+    list<int> l;
+
+    l.push_back(1);
+    l.push_back(2);
+
+    for (int i : l) {
+        cout << i << " ";
+    }
+
+    cout << endl;
+    l.erase(l.begin());
+    cout << "After Erase" << endl;
+    for (int i : l) {
+        cout << i << " " << endl;
+    }
+    cout << "Size of List " << l.size() << endl;
+}
