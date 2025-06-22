db8f42b884ebb43f97533b542e1258a9b8ac71fd
ankurbansal15 <ankurbansal153@gmail.com>
2025-06-22 08:35:04 +0530

    varconst

---
+++ varconst/main.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    
+    int score;
+    score = 110;
+
+    const int uid = 232323;
+
+    int balance = 500;
+    balance = 1000;
+
+    // uid = 1223;
+    cout << "Welcome to cpp 1" << endl ;
+
+    
+    cout << "Welcome to cpp 2" << endl ;
+    cout << "Welcome to cpp 3" << endl ;
+
+    return 0;
+}
